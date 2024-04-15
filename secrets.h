f7be906e3af511b17e34ec22acd0cdb60ca15a56
Khangrt <163809139+Khangrt@users.noreply.github.com>
2024-04-15 15:45:38 +1000

    Add files via upload

---
+++ secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Khangrt"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "11111111"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 2507590		// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "ZQG9NHEVNQCWW1W2"   // replace XYZ with your channel write API Key
